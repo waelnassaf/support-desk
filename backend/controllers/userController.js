205bbbdb731a38198c46cd27b4c4136c791f62a5
Wael <waelassaf121@gmail.com>
2023-03-22 22:34:31 +0300

    Protect Routes & Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,13 @@
-// @desc -  Register a new user.
-// @route   /api/users
-// @access  Public
-
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
 
+// @desc -  Register a new user.
+// @route   /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
 
@@ -42,6 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -53,7 +54,42 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route.')
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    // Check user and passwords match
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
 })
 
-module.exports = {registerUser, loginUser}
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+    }
+    res.status(200).json(user)
+})
+
+
+// Generate token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
+module.exports = {registerUser, loginUser, getMe}
