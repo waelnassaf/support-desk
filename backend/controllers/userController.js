2c036519fdd656ee94bc3d62158a83bebe09d98e
Wael <waelassaf121@gmail.com>
2023-03-22 19:44:55 +0300

    Connect DB and create first user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,10 @@
 // @access  Public
 
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
@@ -12,7 +16,37 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please include all fields.')
     }
-    res.send('Register Route.')
+
+
+    // Find if user already exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new error('Invalid user data')
+    }
 })
 
 // @desc -  User Login
