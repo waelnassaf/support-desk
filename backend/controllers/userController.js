a39c3464bea9e1dd3d869c512076aa0700c79571
Wael <waelassaf121@gmail.com>
2023-03-22 19:17:00 +0300

    Custom error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,25 @@
-const registerUser = (req, res) => {
-    res.send('Register Route.')
-}
+// @desc -  Register a new user.
+// @route   /api/users
+// @access  Public
 
-const loginUser = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields.')
+    }
+    res.send('Register Route.')
+})
+
+// @desc -  User Login
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route.')
-}
+})
 
 module.exports = {registerUser, loginUser}
