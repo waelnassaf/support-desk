23d5971379ec656c2509187bcf683d6328665923
Wael <waelassaf121@gmail.com>
2023-03-22 18:10:41 +0300

    Define routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+//Routes.
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT,() => {
+    console.log(`Server started on ${PORT}`)
+})
