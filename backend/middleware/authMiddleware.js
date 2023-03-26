75f5aadd684633f198fa2960fb1555782495d162
Wael <waelassaf121@gmail.com>
2023-03-26 22:06:00 +0300

    Finish Up Authentication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,7 +29,6 @@ const protect = asyncHandler(async (req, res, next) => {
             throw new Error('Not authorized')
         }
     }
-
     if (!token) {
         res.status(401)
         throw new Error('Not authorized')
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { toast } from "react-toastify"
 import { FaSignInAlt } from "react-icons/fa"
 import { useSelector, useDispatch } from "react-redux"
-import { login } from "../features/auth/authSlice"
+import { login, reset } from "../features/auth/authSlice"
+import { useNavigate } from "react-router-dom"
+import Spinner from "../components/Spinner"
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -13,10 +15,21 @@ function Login() {
     const { email, password } = formData
 
     const dispatch = useDispatch()
-
-    const { user, isLoading, isSuccess, message } = useSelector(
+    const navigate = useNavigate()
+    const { user, isLoading, isSuccess, isError, message } = useSelector(
         (state) => state.auth
     )
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+        //Redirect when logged in.
+        if (isSuccess || user) {
+            navigate("/")
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -34,6 +47,8 @@ function Login() {
         }
         dispatch(login(userData))
     }
+
+    if (isLoading) return <Spinner />
 
     return (
         <>
